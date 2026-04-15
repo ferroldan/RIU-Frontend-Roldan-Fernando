be94c5b1d2d80a588a94e028b016c241634bca8e
Fernando Roldan <froldan@tiendapago.net>
2026-04-15 13:50:53 -0300

    config general, archetecture and i18n

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from './core/services/language.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('RIU-Frontend-Roldan-Fernando');
+  protected readonly languageService = inject(LanguageService);
 }
+
